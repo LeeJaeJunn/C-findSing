3b403bae7777b10d5f29a28c139be16ea0e4dcef
LeeJaeJunn <leejjun00@gmail.com>
2022-01-15 22:21:59 +0900

    🎨 [find-song] 텍스트 수정

--- find.c
+++ find.c
@@ -2,17 +2,11 @@
 #include <string.h>
 
 // track 배열
-char tracks[][80] = {
-  "Everything sucks",
-  "All I Want For Christmas",
-  "Peaches",
-  "To Die For",
-  "Rainy day"
-};
+char tracks[][80] = {"Everything sucks", "All I Want For Christmas", "Peaches",
+                     "To Die For", "Rainy day"};
 
 // 검색 문자열을 가진 노래 제목을 출력하는 함수
-void find_track (char search_for[])
-{
+void find_track(char search_for[]) {
   int i;
   for (i = 0; i < 5; i++) {
     if (strstr(tracks[i], search_for))
@@ -20,10 +14,9 @@ void find_track (char search_for[])
   }
 }
 
-int main()
-{
+int main() {
   char search_for[80];
-  printf("검색할 내용: ");
+  printf("검색할 노래 제목 일부: ");
   scanf("%79s", search_for);
   search_for[strlen(search_for) - 1] = '\0';
   find_track(search_for);
